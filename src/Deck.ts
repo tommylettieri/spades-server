914614fd5c82d591405c83a58e6d9d01364b3e4b
TommyZ87 <zeldafanatic87@yahoo.com>
2020-03-20 01:31:19 -0400

    added deck

---
+++ src/Deck.ts
@@ -0,0 +1,30 @@
+import Card from './Card';
+import Suit from './Suit';;
+
+class Deck {
+    private cards:Card[] = [];
+    constructor() {
+        for(let i: number = 0; i < 52; i++) {
+            let suitIndex = Math.floor(i /13);
+            let value = (i % 13) + 2;
+            let suit:Suit;
+            switch(suitIndex) {
+                case 0:
+                    suit = Suit.SPADE;
+                    break;
+                case 1:
+                    suit = Suit.HEART;
+                    break;
+                case 2:
+                    suit = Suit.CLUB;
+                    break;
+                case 3:
+                    suit = Suit.DIAMOND;
+                    break;
+            }
+            this.cards.push(new Card(value, suit));
+        }
+    }
+}
+
+export default Deck;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Card from "./Card";
 import Suit from "./Suit";
+import Deck from "./Deck";
 
-let card:Card = new Card(2, Suit.CLUB);
-console.log(JSON.stringify(card, null, 2));
+// let card:Card = new Card(2, Suit.CLUB);
+// console.log(JSON.stringify(card, null, 2));
+console.log(JSON.stringify(new Deck(), null, 2));
